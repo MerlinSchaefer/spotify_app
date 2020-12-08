fcb8a2d760fc7f3cae0a1140d76a3d76cef29c74
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2020-12-08 10:44:34 +0100

    refine functions

--- spotifuncs.py
+++ spotifuncs.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
 
-def create_df_current_songs(api_results):
+def create_df_top_songs(api_results):
     """reads in the spotipy query results and returns a DataFrame"""
     track_name = []
     track_id = []
@@ -14,13 +14,15 @@ def create_df_current_songs(api_results):
     duration = []
     popularity = []
     for items in api_results['items']:
+        try:
             track_name.append(items['name'])
             track_id.append(items['id'])
             artist.append(items["artists"][0]["name"])
             duration.append(items["duration_ms"])
             album.append(items["album"]["name"])
             popularity.append(items["popularity"])
-
+        except TypeError:
+            pass
     # Create the final df   
     df = pd.DataFrame({ "track_name": track_name, 
                                 "album": album, 
@@ -48,7 +50,6 @@ def create_df_saved_songs(api_results):
             popularity.append(items["track"]["popularity"])
         except TypeError: 
             pass
-
     # Create the final df   
     df = pd.DataFrame({ "track_name": track_name, 
                              "album": album, 
@@ -78,8 +79,11 @@ def append_audio_features(df,spotify_cred_manager, return_feat_df = False):
     feature_cols = list(audio_features[0].keys())[:-7]
     features_list = []
     for features in audio_features:
+        try:
             song_features = [features[col] for col in feature_cols]
             features_list.append(song_features)
+        except TypeError:
+            pass
     df_features = pd.DataFrame(features_list,columns = feature_cols)
     df = pd.concat([df,df_features],axis = 1)
     if return_feat_df == False:
@@ -89,23 +93,26 @@ def append_audio_features(df,spotify_cred_manager, return_feat_df = False):
 
 
 
-def create_similarity_score(df,similarity_score = "linear"):
-    features = list(df.columns[6:])
-    df_features = df[features]
-    df_features_scaled = StandardScaler().fit_transform(df_features)
-    indices = pd.Series(df.index, index = df['track_name']).drop_duplicates()
+def create_similarity_score(df1,df2,similarity_score = "linear"):
+    assert list(df1.columns[6:]) == list(df2.columns[6:])
+    features = list(df1.columns[6:])
+    df_features1,df_features2 = df1[features],df2[features]
+    scaler = StandardScaler()
+    df_features_scaled1,df_features_scaled2 = scaler.fit_transform(df_features1),scaler.fit_transform(df_features2)
+    #indices = pd.Series(df.index, index = df['track_name']).drop_duplicates()
     if similarity_score == "linear":
-        linear_sim = linear_kernel(df_features_scaled, df_features_scaled)
+        linear_sim = linear_kernel(df_features_scaled1, df_features_scaled2)
         return linear_sim
     elif similarity_score == "cosine_sim":
-        cosine_sim = cosine_similarity(df_features_scaled, df_features_scaled)
+        cosine_sim = cosine_similarity(df_features_scaled1, df_features_scaled2)
         return cosine_sim
     ##implement other measures
 
-def get_recommendations(df,song_title, similarity_score):
+def get_recommendations(df,song_title, similarity_score, num_recommends = 5):
+    indices = pd.Series(df.index, index = df['track_name']).drop_duplicates()
     idx = indices[song_title]
     sim_scores = list(enumerate(similarity_score[idx]))
     sim_scores = sorted(sim_scores, key = lambda x: x[1],reverse = True)
-    top_scores = sim_scores[1:6]
+    top_scores = sim_scores[1:num_recommends+1]
     song_indices = [i[0] for i in top_scores]
     return df["track_name"].iloc[song_indices]
