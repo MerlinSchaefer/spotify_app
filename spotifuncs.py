15053b60036c3a47bfc0993bc06d4a1459b8ad9e
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2020-12-15 13:03:44 +0100

    write docstrings for spotifuncs

--- spotifuncs.py
+++ spotifuncs.py
@@ -8,6 +8,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
 def authenticate(redirect_uri, client_cred_manager, username, scope,client_id,client_secret):
+    """
+    Authenticates a user for a given spotify app.
+
+    Parameters
+    ----------
+    redirect_uri : the redirect uri of the spotify app
+    client_cred_manager : SpotifyClientCredentials containing client_id and client_secret
+    username: spotify username
+    scope: authorization scopes as a string
+    client_id: spotify app client_id 
+    client_secret: spotify app client_secret
+
+    Returns
+    -------
+    sp: Spotify auth-token (SpotifyOAuth)
+
+    """
+
     sp = spotipy.Spotify(client_credentials_manager = client_cred_manager)
     token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
     if token:
@@ -18,13 +36,27 @@ def authenticate(redirect_uri, client_cred_manager, username, scope,client_id,cl
 
 
 def create_df_top_songs(api_results):
-    """reads in the spotipy query results and returns a DataFrame"""
+    """
+    Reads in the spotipy query results for user top songs and returns a DataFrame with
+    track_name,track_id, artist,album,duration,popularity
+
+    Parameters
+    ----------
+    api_results : the results of a query to spotify with .current_user_top_tracks()
+
+    Returns
+    -------
+    df: DataFrame containing track_name,track_id, artist,album,duration,popularity
+
+    """
+    #create lists for df-columns
     track_name = []
     track_id = []
     artist = []
     album = []
     duration = []
     popularity = []
+    #loop through api_results
     for items in api_results['items']:
         try:
             track_name.append(items['name'])
@@ -46,12 +78,27 @@ def create_df_top_songs(api_results):
     return df
 
 def create_df_saved_songs(api_results):
+    """
+    Reads in the spotipy query results for user saved songs and returns a DataFrame with
+    track_name,track_id, artist,album,duration,popularity
+
+    Parameters
+    ----------
+    api_results : the results of a query to spotify with .current_user_saved_tracks()
+
+    Returns
+    -------
+    df: DataFrame containing track_name,track_id, artist,album,duration,popularity
+
+    """
+    #create lists for df-columns
     track_name = []
     track_id = []
     artist = []
     album = []
     duration = []
     popularity = []
+    #loop through api_results
     for items in api_results["items"]:
         try:
             track_name.append(items["track"]['name'])
@@ -75,11 +122,37 @@ def create_df_saved_songs(api_results):
 
 
 def top_artists_from_API(api_results):
+    """
+    Reads in the spotipy query results for user top artists and returns a DataFrame with
+    name, id, genres, popularity and uri
+
+    Parameters
+    ----------
+    api_results : the results of a query to spotify with .current_user_top_artists()
+
+    Returns
+    -------
+    df: DataFrame containing name, id, genres, popularity and uri
+
+    """
     df = pd.DataFrame(api_results["items"])
     cols = ["name","id","genres","popularity","uri"]
     return df[cols]
     
 def create_df_recommendations(api_results):
+    """
+    Reads in the spotipy query results for spotify recommended songs and returns a 
+    DataFrame with track_name,track_id,artist,album,duration,popularity
+
+    Parameters
+    ----------
+    api_results : the results of a query to spotify with .recommendations()
+
+    Returns
+    -------
+    df: DataFrame containing track_name, track_id, artist, album, duration, popularity
+
+    """
     track_name = []
     track_id = []
     artist = []
@@ -105,12 +178,24 @@ def create_df_recommendations(api_results):
 
     return df
 
-def append_audio_features(df,spotify_cred_manager, return_feat_df = False):
-    """ Fetches the audio features for all songs in a DataFrame and
-    appends these as rows to the DataFrame
-    Requires spotipy to be set up with an auth token"""
+def append_audio_features(df,spotify_auth, return_feat_df = False):
+    """ 
+    Fetches the audio features for all songs in a DataFrame and
+    appends these as rows to the DataFrame.
+    Requires spotipy to be set up with an auth token.
 
-    audio_features = spotify_cred_manager.audio_features(df["track_id"][:])
+    Parameters
+    ----------
+    df : Dataframe containing at least track_name and track_id for spotify songs
+
+
+    Returns
+    -------
+    df: DataFrame containing all original rows and audio features for each song
+
+    df_features(optional): DataFrame containing just the audio features
+    """
+    audio_features = spotify_auth.audio_features(df["track_id"][:])
     assert len(audio_features) == len(df["track_id"][:])
     feature_cols = list(audio_features[0].keys())[:-7]
     features_list = []
@@ -129,7 +214,26 @@ def append_audio_features(df,spotify_cred_manager, return_feat_df = False):
 
 
 
-def create_similarity_score(df1,df2,similarity_score = "linear"):
+def create_similarity_score(df1,df2,similarity_score = "cosine_sim"):
+    """ 
+    Creates a similarity matrix for the audio features of two Dataframes.
+
+    Parameters
+    ----------
+    df1 : DataFrame containing track_name,track_id, artist,album,duration,popularity
+            and all audio features
+    df2 : DataFrame containing track_name,track_id, artist,album,duration,popularity
+            and all audio features
+    
+    similarity_score: similarity measure (linear,cosine_sim)
+
+    Returns
+    -------
+    df: DataFrame containing all original rows and audio features for each song
+
+    df_features(optional): DataFrame containing just the audio features
+    """
+    
     assert list(df1.columns[6:]) == list(df2.columns[6:])
     features = list(df1.columns[6:])
     df_features1,df_features2 = df1[features],df2[features]
@@ -142,9 +246,27 @@ def create_similarity_score(df1,df2,similarity_score = "linear"):
     elif similarity_score == "cosine_sim":
         cosine_sim = cosine_similarity(df_features_scaled1, df_features_scaled2)
         return cosine_sim
-    ##implement other measures
+    #other measures may be implemented in the future
 
+###still a work in progress
 def get_recommendations(df,song_title, similarity_score, num_recommends = 5):
+    """ 
+    Gives top num_recommends recommendations for a song based on a similarity_score or matrix
+
+    Parameters
+    ----------
+    df : DataFrame containing at least track_name
+
+    song_title : Song from df to get recommendations for.
+
+    num_recommends: number of songs to recommend.
+    
+    similarity_score: similarity matrix
+
+    Returns
+    -------
+    Song recommendations for song_title
+    """
     indices = pd.Series(df.index, index = df['track_name']).drop_duplicates()
     idx = indices[song_title]
     sim_scores = list(enumerate(similarity_score[idx]))
@@ -152,3 +274,4 @@ def get_recommendations(df,song_title, similarity_score, num_recommends = 5):
     top_scores = sim_scores[1:num_recommends+1]
     song_indices = [i[0] for i in top_scores]
     return df["track_name"].iloc[song_indices]
+###still a work in progress
