ea3a9a8c367eff3e325c4cce14fd47c614f12cd3
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2021-01-07 11:20:28 +0100

    create Template for Playlist Buddy

--- Template-PlaylistBuddy.py
+++ Template-PlaylistBuddy.py
@@ -9,38 +9,42 @@ from spotifuncs import authenticate, create_df_playlist, create_df_recommendatio
 
 
 #set path
-path = Path("C:/Users/ms101/OneDrive/DataScience_ML/projects/spotify_app")
+path = Path("-YOUR PATH (SHOULD CONTAIN spotifuncs.py)-")
 
 #get client credentials and user name
-with open(path / "client_s.txt") as f:
-    content = f.readlines()
-content = [x.strip() for x in content]
 
-client_id = content[0]
-client_secret = content[1]
+#### YOU CAN CREATE A .txt with the credentials and read them with the following code if you don't want to enter them directly####
+# with open(path / "client_s.txt") as f:
+#     content = f.readlines()
+# content = [x.strip() for x in content]
+# client_id = content[0]
+# client_secret = content[1]
 
-with open(path / "usernames.txt") as f:
-    usernames = f.readlines()
-usernames = [x.strip() for x in usernames]
-
-username1 = usernames[0] #could be made a user input, may however require new authentication
+client_id = "-YOUR CLIENT_ID-"
+client_secret = "-YOUR CLIENT_SECRET-"
+#### YOU CAN CREATE A .txt with the username(s) and read them with the following code if you don't want to enter them directly####
+# with open(path / "usernames.txt") as f:
+#     usernames = f.readlines()
+# usernames = [x.strip() for x in usernames]
+# username = usernames[0] #could be made a user input, may however require new authentication
+username = "-YOUR USERNAME-"
 
 #set scope, uri and client_credentials_manager
-scope = "user-library-read user-read-recently-played user-top-read playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative"
+scope = "user-library-read user-read-recently-played user-top-read playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative" #can be reduced depending on your playlists
 
-redirect_uri = "https://developer.spotify.com/dashboard/applications/4a4e029d299a4241873db8300038bf0a"
+redirect_uri = "-YOUR REDIRECT_URI FROM YOUR SPOTIFY DEVELOPER DB-"
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, 
                                                       client_secret=client_secret)
 
 #authenticate user
-sp_m = authenticate(redirect_uri, client_credentials_manager, username1, scope, client_id, client_secret)
+sp = authenticate(redirect_uri, client_credentials_manager, username, scope, client_id, client_secret)
 
 #get playlist based on uri input
 
 playlist_uri = input("Please paste in the URI of the playlist you wish to add songs to   ")
-playlist = sp_m.playlist(playlist_uri)
-playlist_df = create_df_playlist(playlist,sp = sp_m)
+playlist = sp.playlist(playlist_uri)
+playlist_df = create_df_playlist(playlist,sp = sp)
 
 #create mean_song
 mean_song = pd.DataFrame(columns=playlist_df.columns)
@@ -51,16 +55,17 @@ seed_tracks = playlist_df["track_id"].tolist()
 #create recommendation df
 recomm_dfs = []
 for i in range(5,len(seed_tracks)+1,5):
-    recomms = sp_m.recommendations(seed_tracks = seed_tracks[i-5:i],limit = 25)
-    recomms_df = append_audio_features(create_df_recommendations(recomms),sp_m)
+    recomms = sp.recommendations(seed_tracks = seed_tracks[i-5:i],limit = 25) #limit could be modified to get more or less recommendations per song in the org. playlist
+    recomms_df = append_audio_features(create_df_recommendations(recomms),sp)
     recomm_dfs.append(recomms_df)
 recomms_df = pd.concat(recomm_dfs)
 recomms_df.reset_index(drop = True, inplace = True)
+
 #create similarity scoring between playlist and recommendations
 similarity_score = create_similarity_score(playlist_df,recomms_df)
 while True:
     #get a filtered recommendations df
-    final_recomms = recomms_df.iloc[[np.argmax(i) for i in similarity_score]]
+    final_recomms = recomms_df.iloc[[np.argmax(i) for i in similarity_score]] #get indeces of most similar songs
     final_recomms = final_recomms.drop_duplicates()
     #filter again so tracks are not already in playlist_df
     final_recomms = final_recomms[~final_recomms["track_name"].isin(playlist_df["track_name"])]
@@ -86,8 +91,6 @@ while True:
         break
         
 
-
-
 #filter with mean song or sample from recommended
 n_recommendations = int(input("how many songs would you like to add to your playlist? Please enter a number between 1 - 20   "))
 assert 21 > n_recommendations > 0 , "Number of Recommendations must be between 1 and 20"
@@ -102,6 +105,6 @@ else:
 # add songs to playlist
 confirm = input("Please confirm that you want to add songs to the playlist by typing YES   ")
 if confirm == "YES":
-    sp_m.user_playlist_add_tracks(username1,
+    sp.user_playlist_add_tracks(username,
                               playlist_id = playlist_uri,
                               tracks = final_recomms["track_id"].tolist())
